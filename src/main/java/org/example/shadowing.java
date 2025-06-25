8dfd25ee2c7f2a98ccb338a37c7953e3916743b0
Ayush Koli <ayushkoli0709@gmail.com>
2025-06-25 11:37:19 +0530

    shadowing

--- src/main/java/org/example/shadowing.java
+++ src/main/java/org/example/shadowing.java
@@ -1,4 +1,17 @@
 package org.example;
 
 public class shadowing {
+    static int x=20;
+    public static void main(String[] args) {
+
+
+            System.out.println(x);
+             int x=33; //new x shadows the higher level scope x and i available in this class only
+            System.out.println(x);
+
+        print();
+    }
+    static void print(){
+        System.out.println(x);
+    }
 }
